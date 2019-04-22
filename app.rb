bf708aa6ddaf6def6561939e3694aa34bdd03c10
ryanden2018 <denlinger.ryan@gmail.com>
2019-04-22 13:54:28 -0500

    Nick and Ryan

--- app.rb
+++ app.rb
@@ -1,8 +1,25 @@
 class App < Sinatra::Base
 
+
+
 	get '/' do
 		erb :index
 	end
+
+  get '/hello' do
+    erb :hello
+  end
+
+  get '/date' do
+    @days = ["Sunday","Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday"]
+    @month = ["Nomonth", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    erb :date
+  end
+
+  get '/goodbye' do
+    @name = "Joe"
+    erb :goodbye
+  end
 
 
 end
